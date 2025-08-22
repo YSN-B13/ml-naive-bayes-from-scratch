c319c096d792b5fc5e2e3c4bcd20139899161af9
YASSINE B13 <141883763+YSN-B13@users.noreply.github.com>
2025-08-22 15:50:11 +0100

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+from gaussian_nb import GaussianNB
 
+# Load Iris dataset
+df = pd.read_csv("C:/Users/hp/Downloads/iris.data.csv", header=None)
+df.columns = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+
+# Features & labels
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train model
+model = GaussianNB()
+model.fit(X_train, y_train)
+
+# Predictions
+y_pred = model.predict(X_test)
+
+# Evaluation
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
